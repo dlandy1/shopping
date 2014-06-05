e3c632cc9598ac3f56b9975ebe0fde2c69ddecc3
dlandy1 <DanLandy@Daniels-MacBook-Pro-6.local>
2014-06-05 00:36:02 -0400

    working product

--- js/app.js
+++ js/app.js
@@ -1,21 +1,33 @@
 $(document).ready(function(){
-	var check = $("<div class=check></div>")
-	var done = $("<div class=done></div>")
-	var image = $("<div class=image></div>")
+	var check = '<span class="check"></span>'
+	var done = '<span class="done"></span>'
+	var image = '<span class="image"></span>'
 $( "input" ).keyup(function(event) {
     var value = $( this ).val();
+    var work = '<li class="new">'+ check + value + image + '</li>';
     if (event.keyCode == 13 && value.length > 0) {
-    		$( "ul" ).prepend("<li></li>");
-    		$("li:first-child").text(value).show();
-    		$("li").append(check);
-    		$("li").append(image);
+    		$( "ul" ).prepend(work);
     		$("li").show();
-    		$(".new").hide();
+    		$("li:last-child").hide();
     		$("input").val('');
     	};
   })
-	$(check).mouseup(function(){
-		$("li").append(done);
-		$(this).remove();
+	$(document).on("click", ".image", function(){
+     $(this).closest('li').fadeOut(300).remove();
+	});
+	$(document).on("click", ".check", function(){
+		if (!$(this).closest('li').hasClass('a1')) {
+			$(this).closest('li').addClass("a1");
+			$(this).addClass('done');
+			$(this).removeClass('check');
+		}
+		else {
+			$(this).closest('li').removeClass("a1");
+		}
 	})
+	$(document).on("click", ".done", function(){
+			$(this).closest('li').removeClass("a1");
+			$(this).addClass('check');
+			$(this).removeClass('done');
+		});
 });
